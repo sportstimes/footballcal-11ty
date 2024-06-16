8bedde71b5cc139ceb6c81d160560ddc4db0e930
Trevor Morris <code@trovster.com>
2024-06-16 16:11:17 +0100

    fix: generate events.ics file

---
+++ src/events.11ty.js
@@ -0,0 +1,50 @@
+const ics = require('ics')
+const dateTime = require('moment')
+const absoluteUrl = require('./_filters/absoluteUrl')
+
+module.exports = class FeedTemplate {
+  data() {
+    return {
+      layout: null,
+      permalink: '/events.ics',
+    }
+  }
+
+  render({ config, collections }) {
+    const events = collections['webcal-game'].map(function (game) {
+      const event = {
+        start: FeedTemplate.date(game.data.date),
+        title: game.data.title,
+        description: FeedTemplate.description(game),
+        location: game.data.locationName,
+        url: absoluteUrl(game.page.url, config.baseUrl),
+        status: 'CONFIRMED',
+        categories: game.data.tags
+      }
+
+      if (game.data.endDate) {
+        event.end = FeedTemplate.date(game.data.endDate)
+      } else {
+        event.duration = { hours: 1, minutes: 45 }
+      }
+
+      return event
+    })
+
+    return ics.createEvents(events).value
+  }
+
+  static date(date) {
+    return dateTime(date).format('YYYY-M-D-H-m').split('-').map(a => parseInt(a, 10))
+  }
+
+  static description(game) {
+    let description = game.content.replace(/(<([^>]+)>)/gi, '').replace(/\n$/, '')
+
+    if (game.data.tv !== undefined) {
+      description = `TV Channels: ${game.data.tv.join(', ')}\r\n\r\n${description}`
+    }
+
+    return description
+  }
+}
