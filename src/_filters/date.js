f26aafa62869376a76b6b1e64d35c95a39bf3800
Trevor Morris <code@trovster.com>
2024-08-20 13:39:29 +0100

    fix: add ordinal to dates

--- src/_filters/date.js
+++ src/_filters/date.js
@@ -1,5 +1,12 @@
 const { DateTime } = require('luxon')
 
-module.exports = (date, format) => {
-  return DateTime.fromISO(date).toFormat(format)
+function ordinal (n) {
+  const s = ['th', 'st', 'nd', 'rd']
+  const v = n % 100
+  return "'" + n + (s[(v - 20) % 10] || s[v] || s[0]) + "' "
+}
+
+module.exports = (date, format, locale = 'en') => {
+  date = DateTime.fromISO(date).setLocale(locale)
+  return date.toFormat(format.replace('dS ', ordinal(date.day)))
 }
